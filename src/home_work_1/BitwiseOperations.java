e3859bcc5c4dec78d3cbd4d5397db5f6e3faae4a
Roman Gulyako <romangulyako@gmail.com>
2024-06-25 21:39:23 +0300

    Оптимизировал класс BitwiseOperations.java и "причесал" в нем код

--- src/home_work_1/BitwiseOperations.java
+++ src/home_work_1/BitwiseOperations.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class BitwiseOperations {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Input x number: ");
+
+        System.out.print("Input X number: ");
         int x = input.nextInt();                        // 13 - 0000_1101
-        System.out.print("Input y number: ");
+        System.out.print("Input Y number: ");
         int y = input.nextInt();                        // 27 - 0001_1011
         input.close();
-        int resultAnd = x & y;                          /*    0000_1101
+
+        System.out.println("x & y = " + (x & y));       /*    0000_1101
                                                             & 0001_1011
                                                             -----------
                                                               0000_1001 - 9 */
-        System.out.println("x & y = " + resultAnd);
-        int resultOr = x | y;                           /*    0000_1101
+
+        System.out.println("x | y = " + (x | y));       /*    0000_1101
                                                             | 0001_1011
                                                             -----------
                                                               0001_1111 - 31 */
-        System.out.println("x | y = " + resultOr);
     }
 }
