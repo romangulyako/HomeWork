dbdead4eaac1364037e80ecae8904ceb5cd55b41
Roman Gulyako <romangulyako@gmail.com>
2024-06-25 21:46:15 +0300

    Оптимизировал класс ByteConverter.java и "причесал" в нем код

--- src/home_work_1/ByteConverter.java
+++ src/home_work_1/ByteConverter.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 public class ByteConverter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Чтобы перевести кБ в Б нажмите 1, Б в кБ нажмите 2: ");
+        System.out.print("Чтобы перевести кБ в Б нажмите 1, Б в кБ нажмите другую цифру: ");
         int choice = input.nextInt();
+
         if (choice == 1) {
             System.out.print("Введите количество килобайт: ");
             int kBytes = input.nextInt();
-            System.out.println(kBytes + " кБ = " + kBytes * 1024 + " B");
-        }
-        else if (choice == 2) {
-            System.out.print("Введите количество байт: ");
-            int bytes = input.nextInt();
-            System.out.println(bytes + " Б = " + bytes / 1024.0 + " кB");
+            System.out.println(kBytes + " кБ = " + kBytes * 1024 + " Б");
         }
         else {
-            System.out.print("Такого варианта не существует");
+            System.out.print("Введите количество байт: ");
+            int bytes = input.nextInt();
+            System.out.println(bytes + " Б = " + bytes / 1024.0 + " кБ");
         }
         input.close();
     }
