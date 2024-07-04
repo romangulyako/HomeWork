c6fb9dfe63825d5c35592b493a3bef8891f2a348
Roman Gulyako <romangulyako@gmail.com>
2024-07-05 00:00:45 +0300

    Решение задачи 1.2 (перемножить цифры переданного числа)

---
+++ src/home_work_2/loops/MultiplyDigits.java
@@ -0,0 +1,44 @@
+package home_work_2.loops;
+
+import home_work_2.utils.ConsoleUtils;
+
+public class MultiplyDigits {
+    public static void main(String[] args) {
+
+        StringBuilder result = new StringBuilder();
+
+        if(ConsoleUtils.isInt(args[0])) {                                               // Проверка, корректно ли передано в аргументы целое число
+            int number = Integer.parseInt(args[0]);
+
+            char[] arr = args[0].toCharArray();
+            for (int i = 0; i < arr.length; i++) {
+                if (i!= arr.length - 1) {
+                    result.append(arr[i]);
+                    result.append(" * ");
+                } else {
+                    result.append(arr[i]);
+                    result.append(" = ");
+                }
+            }
+
+            result.append(multiply(number)) ;                                       // Вызов метода, перемножающего все цифры переданного числа
+        } else if(ConsoleUtils.isDouble(args[0])) {                                     // Проверка, передано ли в аргументы вещественное число
+            result.append("Введено не целое число");
+        } else {
+            result.append("Введено не число");
+        }
+
+        System.out.println(result.toString());
+
+
+    }
+
+    public static long multiply (int number) {                                      // Метод, перемножающий цифры переданного в него числа
+        int result = 1;
+        while (number > 0) {
+            result *= (number % 10);
+            number /= 10;
+        }
+        return result;
+    }
+}
