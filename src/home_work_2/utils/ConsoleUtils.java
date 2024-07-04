d5ab5bf57dd9cdb2d978ea4857cb8f3ab33e07fd
Roman Gulyako <romangulyako@gmail.com>
2024-07-04 23:59:48 +0300

    Добавил метод, проверяющий, содержит ли строка только число типа double

--- src/home_work_2/utils/ConsoleUtils.java
+++ src/home_work_2/utils/ConsoleUtils.java
@@ -1,10 +1,20 @@
 package home_work_2.utils;
 
 public class ConsoleUtils {
-    // метод проверяет, корректно ли в строку записано целое число
+    // метод проверяет, корректно ли в строку записано число типа int
     public static boolean isInt(String row) throws NumberFormatException {
         try {
             Integer.parseInt(row);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    // метод проверяет, корректно ли в строку записано число типа double
+    public static boolean isDouble(String row) throws NumberFormatException {
+        try {
+            Double.parseDouble(row);
             return true;
         } catch (NumberFormatException e) {
             return false;
