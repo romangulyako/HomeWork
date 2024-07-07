e1ae522a7886538b4997617a256767f7ae512795
Roman Gulyako <romangulyako@gmail.com>
2024-07-08 00:48:31 +0300

    Решение задания 1.4.* (Умножать число long a = 1; пока не произойдёт переполнение)

---
+++ src/home_work_2/loops/MultiplyUntilOverflow.java
@@ -0,0 +1,39 @@
+package home_work_2.loops;
+
+import home_work_2.utils.ConsoleUtils;
+
+public class MultiplyUntilOverflow {
+    public static void main(String[] args) {
+        boolean isExit = false;
+
+        while (!isExit) {
+            System.out.println("Введите 0 если хотите выйти из программы");
+            System.out.print("Введите множитель: ");
+            int factor = Integer.parseInt(ConsoleUtils.inputFromConsole());
+
+            if(factor != 0) {
+                System.out.println("Значение до переполнения: " + multiplyUntilOverflow(factor));
+                System.out.println("Значение после переполнения: " +valueAfterOverflow(factor));
+            } else {
+                isExit = true;
+            }
+        }
+    }
+
+    public static long multiplyUntilOverflow (int factor) {
+        long a = 1;
+        try {
+            while (true) {
+                a = Math.multiplyExact(a,factor);
+            }
+        } catch (ArithmeticException e) {
+
+        }
+
+        return  a;
+    }
+
+    public static long valueAfterOverflow(int factor) {
+        return multiplyUntilOverflow(factor) * factor;
+    }
+}
