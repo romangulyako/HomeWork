b94e7d3e02eb5428dfc826f691e353e4b88a146a
Roman Gulyako <romangulyako@gmail.com>
2024-07-24 14:59:50 +0300

    Поправил документацию в классе CalculatorWithMathExtends

--- src/home_work_3/calcs/simple/CalculatorWithMathExtends.java
+++ src/home_work_3/calcs/simple/CalculatorWithMathExtends.java
@@ -2,12 +2,14 @@ package home_work_3.calcs.simple;
 
 public class CalculatorWithMathExtends extends CalculatorWithOperator {
     /**
-     * Метод возводит дробное число в степень
+     * Переопределяет метод pow из класса CalculatorWithOperator
+     * и возводит дробное число в степень с помощью библиотеки Math
      *
      * @param x - число, которое нужно возвести
      * @param y - степень возведения
      * @return результат возведения числа x в степень y
      */
+    @Override
     public double pow(double x, int y) {
         if (x > 0 && y >= 0) {
             return Math.pow(x,y);
@@ -17,21 +19,25 @@ public class CalculatorWithMathExtends extends CalculatorWithOperator {
     }
 
     /**
-     * Метод возвращает модуль числа
+     * Переопределяет метод abs из класса CalculatorWithOperator
+     * и возвращает модуль числа с помощью библиотеки Math
      *
      * @param x - число, которое нужно вернуть по модулю
      * @return модуль числа x
      */
+    @Override
     public int abs(int x) {
         return Math.abs(x);
     }
 
     /**
-     * Метод вычисляет квадратный корень из числа
+     * Переопределяет метод sqrt из класса CalculatorWithOperator
+     * и вычисляет квадратный корень из числа с помощью библиотеки Math
      *
      * @param x - число, из которого нужно вычислить квадратный корень
      * @return квадратный корень числа x
      */
+    @Override
     public double sqrt(int x) {
         return Math.sqrt(x);
     }
