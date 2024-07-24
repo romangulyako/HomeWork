ae3894f8a999dec889fcedf5ee66c681efa9925a
Roman Gulyako <romangulyako@gmail.com>
2024-07-24 13:13:50 +0300

    Выполнил задание 1.
    Создал класс WithoutCalculatorMain.java, где рассчитал пример из задания и распечатал результат в консоль

---
+++ src/home_work_3/runners/WithoutCalculatorMain.java
@@ -0,0 +1,9 @@
+package home_work_3.runners;
+
+public class WithoutCalculatorMain {
+    public static void main(String[] args) {
+        double result = 4.1 + 15 * 7 + ((28 / 5.0) * (28 / 5.0)); // 140.45999999999998
+
+        System.out.println(result);
+    }
+}
