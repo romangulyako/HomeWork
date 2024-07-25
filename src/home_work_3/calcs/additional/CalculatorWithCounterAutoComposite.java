46daf251b50bcfbb15e7600ba8e2cbe40c2eed63
Roman Gulyako <romangulyako@gmail.com>
2024-07-25 13:27:26 +0300

    Выполнил задание 7.
    Создал класс CalculatorWithCounterAutoComposite. В нем создал поле типа CalculatorWithMathCopy и методы для 7 математических операций. В методах решение математических задач делегируется экземпляру класса CalculatorWithMathCopy, а также реализован подсчет выполненных операций. Добавил метод, который возвращает количество операций. Создал класс CalculatorWithCounterDelegateCompositeMain, в котором протестировал работу класса CalculatorWithCounterAutoComposite

---
+++ src/home_work_3/calcs/additional/CalculatorWithCounterAutoComposite.java
@@ -0,0 +1,113 @@
+package home_work_3.calcs.additional;
+
+import home_work_3.calcs.simple.CalculatorWithMathCopy;
+
+public class CalculatorWithCounterAutoComposite {
+    /**
+     * Поле, хранящее экземпляр калькулятора, описанного классом CalculatorWithMathCopy
+     */
+    CalculatorWithMathCopy calc = new CalculatorWithMathCopy();
+
+    /**
+     * Поле, хранящее количество операций, выполненных калькулятором
+     */
+    private long countOperation = 0;
+
+    /**
+     * Метод возвращет количество операций, выполненных калькулятором
+     *
+     * @return поле countOperation
+     */
+    public long getCountOperation() {
+        return countOperation;
+    }
+
+    /**
+     * Метод делит одно число на другое с помощью экземпляра класса CalculatorWithMathCopy
+     * и увеличивает счетчик выполненных операций калькулятором на 1
+     *
+     * @param x - числитель
+     * @param y - знаменатель
+     * @return результат деления x на y
+     */
+    public double divide(double x, double y) {
+        countOperation++;
+        return calc.divide(x,y);
+    }
+
+    /**
+     * Метод умножает одно число на другое с помощью экземпляра класса CalculatorWithMathCopy
+     * и увеличивает счетчик выполненных операций калькулятором на 1
+     *
+     * @param x - первый множитель
+     * @param y - второй множитель
+     * @return результат умножения x на y
+     */
+    public double multiply(double x, double y) {
+        countOperation++;
+        return calc.multiply(x,y);
+    }
+
+    /**
+     * Метод складывает одно число с другим с помощью экземпляра класса CalculatorWithMathCopy
+     * и увеличивает счетчик выполненных операций калькулятором на 1
+     *
+     * @param x - первое слогаемое
+     * @param y - второе слогаемое
+     * @return результат сложения x и y
+     */
+    public double sum(double x, double y) {
+        countOperation++;
+        return calc.sum(x,y);
+    }
+
+    /**
+     * Метод отнимает от одного числа другое с помощью экземпляра класса CalculatorWithMathCopy
+     * и увеличивает счетчик выполненных операций калькулятором на 1
+     *
+     * @param x - уменьшаемое
+     * @param y - вычитаемое
+     * @return результат вычитания y из x
+     */
+    public double subtract(double x, double y) {
+        countOperation++;
+        return calc.subtract(x,y);
+    }
+
+    /**
+     * Метод возводит дробное число в степень с помощью экземпляра класса CalculatorWithMathCopy
+     * и увеличивает счетчик выполненных операций калькулятором на 1
+     *
+     * @param x - число, которое нужно возвести
+     * @param y - степень возведения
+     * @return результат возведения числа x в степень y
+     */
+    public double pow(double x, int y) {
+        countOperation++;
+        return calc.pow(x,y);
+    }
+
+    /**
+     * Метод возвращает модуль числа с помощью экземпляра класса CalculatorWithMathCopy
+     * и увеличивает счетчик выполненных операций калькулятором на 1
+     *
+     * @param x - число, которое нужно вернуть по модулю
+     * @return модуль числа x
+     */
+    public int abs(int x) {
+        countOperation++;
+        return calc.abs(x);
+    }
+
+    /**
+     * Метод вычисляет квадратный корень из числа с помощью экземпляра класса CalculatorWithMathCopy
+     * и увеличивает счетчик выполненных операций калькулятором на 1
+     *
+     * @param x - число, из которого нужно вычислить квадратный корень
+     * @return квадратный корень числа x
+     */
+    public double sqrt(int x) {
+        countOperation++;
+        return calc.sqrt(x);
+    }
+}
---
+++ src/home_work_3/runners/CalculatorWithCounterDelegateCompositeMain.java
@@ -0,0 +1,14 @@
+package home_work_3.runners;
+
+import home_work_3.calcs.additional.CalculatorWithCounterAutoComposite;
+
+public class CalculatorWithCounterDelegateCompositeMain {
+    public static void main(String[] args) {
+        CalculatorWithCounterAutoComposite calc = new CalculatorWithCounterAutoComposite();
+
+        double result = calc.sum(calc.sum(4.1,calc.multiply(15,7)), calc.pow(calc.divide(28,5), 2));
+
+        System.out.println(result);
+        System.out.println(calc.getCountOperation());
+    }
+}
